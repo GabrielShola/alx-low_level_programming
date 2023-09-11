82bdd19ebb23c5d5e122b013f96377d03e2f8569
GabrielShola <gabrielakinbande@gmail.com>
2023-09-11 13:45:35 +0300

    ew

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - to print using putchar
+ * Return: 0
+ *
+*/
+
+int	main(void)
+{
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar("\n");
+
+	return (0);
+}
