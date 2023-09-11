d70751d64a6b09abaaf799afafaca116be8d2841
GabrielShola <gabrielakinbande@gmail.com>
2023-09-11 15:50:51 +0300

    new

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - to print an alphabet in reverse order
+ *
+ * Return: 0
+ */
+
+int	main(void)
+
+{
+	char i;
+
+	while (i = "z"; z >= a)
+
+	{	putchar(i);
+			i++;
+	}
+	putchar('\n');
+	return (0);
+}
