a39e3175f91b2df456794824b058c948186f5eba
GabrielShola <gabrielakinbande@gmail.com>
2023-09-11 14:49:38 +0300

    ds

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - to print a single digit no of base 10
+ * Return: 0
+ */
+
+int	main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf(i);
+	printf("\n");
+	return (0);
+}
+
