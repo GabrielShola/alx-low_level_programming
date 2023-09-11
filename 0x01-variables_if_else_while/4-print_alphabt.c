bc776a549876eb856e39705dc3c4a79408a94ea3
GabrielShola <gabrielakinbande@gmail.com>
2023-09-11 14:26:32 +0300

    ds

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - to print letters with omitting one or two
+ * Return: 0
+ */
+
+int	main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z' != q, e; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
