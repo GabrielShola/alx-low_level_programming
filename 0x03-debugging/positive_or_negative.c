c8f4a10ef069d4338c6000b9f10bc278f36a76c5
GabrielShola <gabrielakinbande@gmail.com>
2023-09-14 23:44:01 +0300

    ew

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - to check positive or negative
+ * Return: 0
+ */
+
+positive_or_negative(i)
+{
+	if (i > 0)
+	{
+		printf("%i is positive \n", i);
+
+	}
+	else if (i =0)
+	{
+		printf("%i is equal to 0\n", i);
+	}
+	else 
+	{
+		printf("%i is negative \n", i);
+	}
+
+}
+
+
+
