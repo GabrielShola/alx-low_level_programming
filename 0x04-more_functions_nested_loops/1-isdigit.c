d48c32fee5e415060379d522c2a02e68890a4d9d
GabrielShola <gabrielakinbande@gmail.com>
2023-09-16 01:38:45 +0300

    ds

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _isdigit - to checks for a digit
+ * Return: 1 for a success and 0 for a failure
+ * @c: variable text
+ */
+
+int	_isdigit(int c)
+{
+	char c;
+
+	if
+		(c >= 48 && c <= 57);
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+
+
+}
+
+
